9bb20f0d67db76bfe28d2ca840d04fa3652eb148
student <Ahmed Mohammed A Alalyani>
2020-04-25 01:12:19 +0930

    new file:   unit1-solution2.sh 	new file:   unit1-solution2.tgz 	modified:   unit1-exercise-1-grade.sh 	modified:   unit1-exercise-2-grade.sh 	deleted:    unit1-solution1.tgz 	exercise1.txt 	exercise2.txt

---
+++ unit1-solution2.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+mkdir unit1exercise2
+cd unit1exercise2
+sudo install -d -m603 -o news -g floppy verrennkeit
+sudo install -d -m353 -o proxy -g fax besprachtest
+sudo install -d -m073 -o news -g fax enpflums
+sudo install -d -m653 -o mail -g floppy aufgewarfs 
+sudo install -d -m610 -o news -g student aushundheit
+sudo install -d -m337 -o games -g tape verklettt/aufwarfheit/enwarfst
+sudo install -d -m341 -o news -g fax verklettt/aufwarfheit/bestehs
+sudo install -d -m323 -o news -g student angetritter/beschmeckung/ausgesprachs
+sudo install -d -m420 -o proxy -g floppy angetritter/beschmeckung/aufkletts
+sudo install -d -m721 -o nobody -g cdrom angetritter/beschmeckung/ausrauchheit
+sudo install -d -m444 -o games -g dip angetritter/antrittung/ankaeser
+sudo install -d -m243 -o mail -g student enpflumen/beklettte
+sudo install -d -m227 -o nobody -g tape enpflumen/aufgesitzse/zerraucher
+cd enpflumen
+sudo chmod 173 aufgesitzse
+sudo chown news:cdrom aufgesitzse
+cd ..
+sudo chmod 012 enpflumen
+sudo chown news:news enpflumen
+cd angetritter
+sudo chmod 034 beschmeckung
+sudo chown lp:audio beschmeckung
+sudo chmod 116 antrittung
+sudo chown mail:student antrittung
+cd ..
+sudo chmod 507 angetritter
+sudo chown news:tape angetritter 
+cd verklettt
+sudo chmod 725 aufwarfheit
+sudo chown proxy:voice aufwarfheit
+cd ..
+sudo chmod 711 verklettt
+sudo chown proxy:tape verklettt
+cd ..
+sudo chmod 775 unit1exercise2
+sudo tar -cvzf unit1-solution2.tgz unit1exercise2
