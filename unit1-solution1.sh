65719eb615172284b92b4618a7d4e583cda2b61f
student <Ahmed Mohammed A Alalyani>
2020-04-24 22:20:32 +0930

    new file:   unit1-solution1.sh 	new file:   unit1-solution1.tgz 	modified:   unit1-exercise-1-grade.sh 	exercise1.txt

---
+++ unit1-solution1.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+mkdir unit1exercise1
+cd unit1exercise1
+mkdir -m660 enhundst
+mkdir -m003 bepflumheit
+mkdir -m072 zersinnse
+mkdir -m316 besprachst
+mkdir -m400 verrabarbst
+mkdir -p -m231 auswitzer/angetraust/auspflumse
+cd auswitzer
+mkdir -p -m002 angewarfen/aufgesetzheit
+mkdir -p -m336 aufgerabarbt/befahrt
+mkdir -p -m162 aufgerabarbt/einkrauen
+cd ..
+mkdir -p -m572 zertritter/aufklettheit/angekrauung
+mkdir -p -m212 zertritter/aufklettheit/angerabarbheit
+mkdir -p -m205 verkaesen/aufwarftest/aufgerennung
+cd zertritter
+chmod 367 aufklettheit
+cd ..
+chmod 626 zertritter
+cd verkaesen
+chmod 507 aufwarftest
+cd ..
+chmod 774 verkaesen
+cd auswitzer
+chmod 446 angetraust
+chmod 002 angewarfen
+chmod 762 aufgerabarbt
+cd ..
+chmod 500 auswitzer
+cd ..
+sudo chmod 775 unit1exercise1
+sudo tar -cvzf unit1-solution1.tgz unit1exercise1
